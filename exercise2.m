d6c000c08b4686987cb52b3f78545f7f5e7d61d7
Juan R. Pacha <juan.ramon.pacha@upc.edu>
2019-09-19 17:59:21 +0200

    Files exercise2.m and interpTempTriangMesh.m reorganised, and new commets aggregated.

--- exercise2.m
+++ exercise2.m
@@ -1,12 +1,25 @@
+%Exercise 2: Several Triangles
+
+%Let's consider now four triangles adding a central vertex (as it is 
+%shown in the figure). We can define these polygons by the coordinates
+%of the vertices and the triangles defined by these vertices. Then:
+
+%1.-Decidewhich triangle the point p=(0.8,0.7) belongs to. 
+%2.-Assume the temperature at the vertex placed at the point (x,y), 
+%T(x,y), is T(0,0)=10ºC, T(1,0)=20ºC, T(1,1)=30ºC, T(0,1)=40ºC, and 
+%T(0.5,0.5)=50ºC. Then, compute the interpolated temperature at point p.
+
+%Files required: 
+%plotTriangMesh.m: uncompress this file from additionalFiles.zip 
+%baryCoord.m
+
+%Be sure they're placed in the current folder!
+
+%You can download dditionalFiles.zip from Toni Susin's Numerical Factory.
+
+
 clearvars
 close all
-%Exercise 2: Several Triangles
-%Let's consider now four triangles adding a central
-%vertex (as it is shown in the figure). We can define 
-%these polygons by the coordinates of the vertices 
-%and the triangles defined by these vertices. Decide 
-%which triangle the point p=(0.8,0.7) belongs to 
-%
 
 p = [0.8,0.7];
 
@@ -25,8 +38,11 @@ elem=[
     4, 1, 5;
     ];
 
+temp=10:10:50; %Temperatures at the nodes, according to the stated 
+               %global numeration.
+               
 figure()
-plotTriangMesh(nodes,elem);%You can find this function in 
+plotTriangMesh(nodes,elem);
 hold on
 plot(p(1,1),p(1,2),'o','Marker','o',...
     'MarkerFaceColor','red','MarkerSize',6,...
@@ -44,15 +60,13 @@ for e=1:numElem
     vertexsElem=[v1;v2;v3];
     [alphas,isInside]=baryCoord(vertexsElem,p);
     if (isInside >= 1)
-        fprintf(1,'Point p=[%f,%f]\n*Elem: %d\n*Nodes: %d,%d,%d\n',...
-            p,e,n1,n2,n3);
         break;
     end
 end
 
+fprintf(1,'Point p=[%f,%f]\n*Elem: %d\n*Nodes: %d,%d,%d\n',...
+            p,e,n1,n2,n3);
+
 %Interpolation of the temperature
-temp=10:10:50; %Assume T(i)=10*e, e=1,2,3,4,5
 interpTemp=temp(n1)*alphas(1)+temp(n2)*alphas(2)+temp(n3)*alphas(3);
 fprintf('*Interpolated temp.: %.4f%cC\n',interpTemp,char(176));
-
-
