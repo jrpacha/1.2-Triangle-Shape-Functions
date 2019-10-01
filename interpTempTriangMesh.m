9d67222fcb3f5c21965a10deab2fe96a596db709
Juan R. Pacha <juan.ramon.pacha@upc.edu>
2019-10-01 14:04:49 +0200

    Minor changes

--- interpTempTriangMesh.m
+++ interpTempTriangMesh.m
@@ -13,22 +13,15 @@
 clearvars
 close all
 
+p=[0.83,0.7];
+
 eval('meshHole'); %load nodes position and connectivity matrices
                   %from the mesh file meshHole.m 
 
 numNod=size(nodes,1); %number of nodes
 numElem=size(elem,1); %number of elements
 temp=1:numNod; %temperature at each node. Just an example!
-temp=temp';
-p=[0.83,0.7];
-
-figure()
-plotTriangMesh(nodes,elem); %plot nodes and elements 
-hold on
-plot(p(1,1),p(1,2),'o','Marker','o',...
-    'MarkerFaceColor','red','MarkerSize',6,...
-    'lineWidth',2,'color','black')
-hold off
+temp=temp(:);
 
 for e=1:numElem
     %vertexsElem=nodes(elem(e,:),:);
@@ -45,13 +38,33 @@ for e=1:numElem
     end
 end
 
-%Interpolate the temperature
-%interpTemp=alphasElem*temp(elem(e,:));
+%Draw the mesh, and mark the point p and its element's nodes
+figure()
+plotTriangMesh(nodes,elem); %plot nodes and elements 
+hold on
+plot(p(1,1),p(1,2),'ro','Marker','o',...
+    'MarkerFaceColor','red','MarkerSize',5)
+plot(vertexsElem(:,1),vertexsElem(:,2),'og','Marker','o',...
+    'MarkerFaceColor','green','MarkerSize',4)
+hold off
+
+%Interpolate the temperature at point p
 interpTemp=alphasElem(1)*temp(n1)+ ...
     alphasElem(2)*temp(n2)+ ...
     alphasElem(3)*temp(n3);
+%interpTemp=alphasElem*temp(elem(e,:));
 
-fprintf(1,'Point p=[%f,%f]\n',p);
-fprintf(1,'*Element: %d\n',e);
-fprintf(1,'*Nodes: %d,%d,%d\n',n1,n2,n3);
-fprintf(1,'*Interpolated Temp.: %.4f%cC\n',interpTemp,char(176));
+format short e
+e
+nods=[n1,n2,n3]
+vertexsElem
+interpTemp
+
+%Fancy output with fprintf: don't try this at exams!
+fprintf('Elem: %d\n',e)
+fprintf('Nodes: %d,%d,%d\n',n1,n2,n3)
+fprintf('%20s\n','Vertexs Coords.')
+fprintf('%7s%11s\n','X','Y')
+fprintf('%12.5e%12.5e\n',vertexsElem')
+fprintf('\nInterpolated Temp.at point p=(%f,%f):\nT=%12.5e\n',...
+    p,interpTemp);
