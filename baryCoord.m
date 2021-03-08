022db4278f35b2c54ed60c1481f6836e4cef649e
Juan R. Pacha <47285283+jrpacha@users.noreply.github.com>
2021-03-08 17:31:22 +0100

    min(alphas) < -1.0e-10 changed to min(alphas) < -1.0e-14

--- baryCoord.m
+++ baryCoord.m
@@ -27,14 +27,16 @@ c3=A\b;
 Psi1=@(x,y) c1(1)+c1(2)*x+c1(3)*y;
 Psi2=@(x,y) c2(1)+c2(2)*x+c2(3)*y;
 Psi3=@(x,y) c3(1)+c3(2)*x+c3(3)*y;
-
+%
+% Compute barycentric coordinates
+%
 alpha1=Psi1(point(1,1),point(1,2));
 alpha2=Psi2(point(1,1),point(1,2));
 alpha3=Psi3(point(1,1),point(1,2));
 
 alphas=[alpha1,alpha2,alpha3];
 
-if (min(alphas) < -1.0e-10) %at least one of the alphas is negative
+if (min(alphas) < -1.0e-14) %at least one of the alphas is negative
     isInside=0;
 end
 
